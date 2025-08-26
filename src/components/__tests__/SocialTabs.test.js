dd6c724271c18c1e296671c3568779f928697e40
Juan J. Vazquez Gomez <Juanje1019@gmail.com>
2025-08-26 00:49:30 -0400

    Make social tabs responsive on mobile

--- src/components/__tests__/SocialTabs.test.js
+++ src/components/__tests__/SocialTabs.test.js
@@ -1,9 +1,13 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import SocialTabs from '../socials/SocialTabs';
 import { ProfileProvider } from '../../ProfileContext';
+import useMediaQuery from '@mui/material/useMediaQuery';
 import '../../i18n';
 
+jest.mock('@mui/material/useMediaQuery');
+
 test('calls setValue when different tab selected', () => {
+  useMediaQuery.mockReturnValue(false);
   const setValue = jest.fn();
   render(
     <ProfileProvider>
@@ -12,4 +16,16 @@ test('calls setValue when different tab selected', () => {
   );
   fireEvent.click(screen.getByRole('tab', { name: /instagram/i }));
   expect(setValue).toHaveBeenCalledWith('instagram');
+});
+
+test('renders scrollable tabs on small screens', () => {
+  useMediaQuery.mockReturnValue(true);
+  const setValue = jest.fn();
+  const { container } = render(
+    <ProfileProvider>
+      <SocialTabs value="x" setValue={setValue} />
+    </ProfileProvider>
+  );
+  const tabsRoot = container.querySelector('.MuiTabs-root');
+  expect(tabsRoot.className).not.toMatch(/MuiTabs-centered/);
 });
